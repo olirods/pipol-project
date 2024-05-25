8a3026bfe9584518bda072340bac496568756968
Sergio Olivares <sergio.olivares.rodriguez@aon.es>
2024-05-25 19:04:16 +0200

    call hugging face inference api

--- social-analyzer/app.py
+++ social-analyzer/app.py
@@ -6,10 +6,13 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import json
 from twikit import Client
-from transformers import pipeline
 from collections import Counter
 
 load_dotenv()
+
+HG_API_URL = "https://api-inference.huggingface.co/models/daveni/twitter-xlm-roberta-emotion-es"
+hg_api_token = os.environ.get("HG_API_TOKEN")
+hg_headers = {"Authorization": f"Bearer {hg_api_token}"}
 
 app = Flask(__name__)
 ai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
@@ -19,13 +22,12 @@ twitter_client.login(
     auth_info_2=os.environ.get("TWITTER_EMAIL"),
     password=os.environ.get("TWITTER_PASSWORD")
 )
-emotion_analysis = pipeline("text-classification", framework="pt", model="daveni/twitter-xlm-roberta-emotion-es", tokenizer="daveni/twitter-xlm-roberta-emotion-es")
     
 @app.route("/people/<person>/emotions")
 def get_emotions(person):
     tweets = map(lambda tweet: tweet.text, twitter_client.search_tweet(person, 'Top'))
 
-    emotions = list(map(lambda tweet: emotion_analysis(tweet)[0]['label'], tweets))
+    emotions = list(map(lambda tweet: emotion_analysis(tweet)[0][0]['label'], tweets))
 
     return calculate_proportions(emotions)
 
@@ -34,3 +36,9 @@ def calculate_proportions(arr):
     total_items = len(arr)
     proportions = {key: value/total_items for key, value in counter.items()}
     return proportions
+
+def emotion_analysis(tweet):
+    payload = {"inputs": tweet}
+    response = requests.post(HG_API_URL, headers=hg_headers, json=payload)
+	
+    return response.json()
