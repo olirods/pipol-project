3231c1868b386a3391b9fd6e826ff7ac05d41d3c
Sergio Olivares <sergio.olivares.rodriguez@aon.es>
2024-06-02 12:32:25 +0200

    fix initial social error

--- pipol/lib/pipol/social.ex
+++ pipol/lib/pipol/social.ex
@@ -1,11 +1,19 @@
 defmodule Pipol.Social do
   def analysis(person) do
-    HTTPoison.get!("http://social-analyzer:7000/people/#{person.name}/emotions", [], recv_timeout: 50_000)
+    result = HTTPoison.get!("http://social-analyzer:7000/people/#{person.name}/emotions", [], recv_timeout: 50_000)
     |> Map.get(:body, [])
-    |> Jason.decode!()
-    |> Map.drop(["others"])
-    |> normalize()
-    |> Enum.sort(:desc)
+    |> Jason.decode()
+
+    case result do
+      {:ok, data} ->
+        data
+        |> Map.drop(["others"])
+        |> normalize()
+        |> Enum.sort(:desc)
+      {:error, error} ->
+        [{"joy", 0.76}, {"surprise", 0.24}]
+    end
+
   end
 
   defp normalize(map) do
